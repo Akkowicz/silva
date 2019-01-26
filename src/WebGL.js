832089a8d1bcf4abaeec3c054952075515e3d8d0
Mateusz Przybyłowicz <uamfhq@gmail.com>
2019-01-26 13:18:11 +0100

    Add shader support functions and draw a rectangle

--- src/WebGL.js
+++ src/WebGL.js
@@ -5,7 +5,14 @@ function initializeGL() {
     const canvas = document.getElementById("GLCanvas");
     gGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
     if (gGL !== null) {
-        gGL.clearColor(0.0, 0.8, 0.0, 1.0); // set the color to be cleared
+        gGL.clearColor(0.0, 0.8, 0.0, 1.0);
+        // set the color to be cleared
+        // A. initialize the vertex buffer
+        initSquareBuffer(); // This function is defined VertexBuffer.js
+        // B. now load and compile the vertex and fragment shaders
+        initSimpleShader("VertexShader", "FragmentShader");
+        // the two shaders are defined in the index.html file
+        // initSimpleShader() function is defined in ShaderSupport.js
     } else {
         document.write("<br><b>WebGL is not supported!</b>");
     }
@@ -17,5 +24,15 @@ function clearCanvas() {
 
 function doGLDraw() {
     initializeGL();
-    clearCanvas();
+    drawSquare();
+}
+
+function drawSquare() {
+    gGL.clear(gGL.COLOR_BUFFER_BIT);
+    // Step A: Activate the shader to use
+    gGL.useProgram(gSimpleShader);
+    // Step B: Enable the vertex position attribute
+    gGL.enableVertexAttribArray(gShaderVertexPositionAttribute);
+    // Step C: Draw with the above settings
+    gGL.drawArrays(gGL.TRIANGLE_STRIP, 0, 4);
 }
